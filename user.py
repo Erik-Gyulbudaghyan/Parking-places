9a9bde4b32c839240066a53e1fefd88b2836e49c
Erik-Gyulbudaghyan <48749235+Erik-Gyulbudaghyan@users.noreply.github.com>
2019-05-06 13:06:50 +0400

    Update and rename main1.py to user.py

---
+++ user.py
@@ -0,0 +1,75 @@
+import json
+
+places = ("place N1", "place N2", "place N3", "place N4", "place N5")
+times = ("range 0-2", "range 2-5", "range 5-12", "range 12-24", "range 24-more")
+
+def loadLoginData():
+    log = input("choose between user/manager: ")
+    user = "user"
+    manager = "manager"
+    if log == user:
+        print(user)
+    elif log == manager:
+        print(manager)
+    return log
+
+
+def loadSaveNameData():
+    saveName = input("Name and Surname: ")
+    return saveName
+
+
+
+def loadParkPlaceInfo():
+    with open('parking.json') as data_file:
+        parkPlace = json.load(data_file)
+        return parkPlace
+
+
+def loadAskForPlace(initialParkPlaceData):
+    print(places)
+    current_place = input("Choose your parking place: ")
+    if int(current_place) in range(0, 5):
+        current_place_key = "00" + current_place
+        print(initialParkPlaceData["parking_places"]["parking_ID"][current_place_key])
+        return initialParkPlaceData["parking_places"]["parking_ID"][current_place_key]
+    else:
+        print("Your Data Is Wrong")
+        return current_place
+
+def loadAskForTime():
+    print(times)
+    current_time = input("Choose your parking time range: ")
+    return current_time
+
+def printResult(current_time, current_place, saveName, initialParkPlaceData):
+    price = -1
+    if current_time in range(2):
+        price = initialParkPlaceData["parking_places"]["time_range"]["range 0-2"]
+    elif current_time in range(2, 5):
+        price = initialParkPlaceData["parking_places"]["time_range"]["range 2-5"]
+    elif current_time in range(5, 12):
+        price = initialParkPlaceData["parking_places"]["time_range"]["range 5-12"]
+    elif current_time in range(12, 24):
+        price = initialParkPlaceData["parking_places"]["time_range"]["range 12-24"]
+    elif current_time in range(24, 100):
+        price = initialParkPlaceData["parking_places"]["time_range"]["range 24-100"]
+
+    if (price > -1 ):
+        print("Your Place and Time are: " + saveName, current_place, str(current_time) + " hours: " + str(price) + " AMD")
+    else:
+        print("Your Data is wrong")
+        return printResult
+
+def main():
+    log = loadLoginData()
+    saveName = loadSaveNameData()
+    print(saveName)
+    parkPlace = loadParkPlaceInfo()
+    if log == 'user':
+        current_place = loadAskForPlace(parkPlace)
+        print(current_place)
+        current_time = loadAskForTime()
+        print(current_time)
+        printResult(int(current_time), current_place, saveName, parkPlace)
+main()
