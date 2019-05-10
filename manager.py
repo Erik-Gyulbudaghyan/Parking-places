60ac61732de42501c2f13aff0d6df356532f9c0d
Erik-Gyulbudaghyan <48749235+Erik-Gyulbudaghyan@users.noreply.github.com>
2019-05-10 13:10:49 +0400

    Update manager.py

--- manager.py
+++ manager.py
@@ -25,6 +25,26 @@ def loadSaveData(initialUsedPlaceData):
         print(save_data)
         return initialUsedPlaceData["used_place"]
 
+def loadAskForChange():
+    change = input("Do you want to make any changes (Yes/Not): ")
+    while change == "Not" or "No" or "not" or "no":
+        print("Goodbye: ")
+        break
+    if change == "Yes" or "yes":
+        print(change)
+        return change
+
+def loadParkPlaceInfo():
+    with open('parking.json') as data_file:
+        parkPlace = json.load(data_file)
+        return parkPlace
+
+def loadTimedata(initialParkPlaceData, change):
+    if change == "Yes" or "yes":
+        time_change = initialParkPlaceData["parking_place"]["Time_range"]
+        print(time_change)
+
+
 
 def main():
     log = loadLoginData()
@@ -34,6 +54,12 @@ def main():
     if log == 'manager':
         save_data = loadSaveData(usedPlace)
         print(save_data)
+        change = loadAskForChange()
+        print(change)
+        if change == "Yes" or "yes":
+            parkPlace = loadParkPlaceInfo()
+            time_change = loadTimedata(parkPlace, change)
+            print(time_change)
 
 
 main()
