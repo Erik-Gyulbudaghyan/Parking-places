4819e8d0962695c5cb929d123790f3a799a5dfdc
Erik-Gyulbudaghyan <48749235+Erik-Gyulbudaghyan@users.noreply.github.com>
2019-05-10 12:43:07 +0400

    Update manager.py

--- manager.py
+++ manager.py
@@ -14,13 +14,26 @@ def loadSaveNameData():
     saveName = input("Name and Surname: ")
     return saveName
 
+def loadUsedPlaceInfo():
+    with open('usedPlace.json') as data_file:
+        usedPlace = json.load(data_file)
+        return usedPlace
 
-
+def loadSaveData(initialUsedPlaceData):
+    with open('usedPlace.json', 'r') as file:
+        save_data = json.load(file)
+        print(save_data)
+        return initialUsedPlaceData["used_place"]
 
 
 def main():
     log = loadLoginData()
     saveName = loadSaveNameData()
     print(saveName)
-    
+    usedPlace = loadUsedPlaceInfo()
+    if log == 'manager':
+        save_data = loadSaveData(usedPlace)
+        print(save_data)
+
+
 main()
